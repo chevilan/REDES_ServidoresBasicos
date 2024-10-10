bf16724ffe26fcbc278fe7253e4993456a48dc2c
chevilan <jaime.romero@rai.usc.es>
2024-10-10 10:29:33 +0200

    funciona

--- cliente.c
+++ cliente.c
@@ -12,23 +12,44 @@
 
 
 int main(int argc, char **argv){
+    char ipservidor[MAX];
+    int numpuerto = 8080;
+    if(argc > 2){
+        strcpy(ipservidor,argv[1]);
+        numpuerto = atoi(argv[2]);
+    }
+    int socketCliente, sockservidor;
+    struct sockaddr_in dirCliente, dirservidor;
+    socklen_t tamano;
+    char mensaje[MAX];
 
 
-   int socketCliente;
-   struct sockaddr_in dirCliente;
-   socklen_t tamano;
-   char mensaje[MAX];
+    sockservidor = socket(AF_INET, SOCK_STREAM, 0); //creamos el socket del servidor
+    if(sockservidor < 0){ //si es menor que 0 (-1) da error
+        perror("Error al abrir el socket");
+        exit(EXIT_FAILURE);
+    }
+    if (inet_pton(AF_INET, ipservidor, &dirservidor.sin_addr) <= 0) {
+        perror("inet_pton error");
+        exit(EXIT_FAILURE);}
+    // Inicializar direccionserv
+    dirservidor.sin_family = AF_INET; // familia de direcciones//cualquier ip 
+    dirservidor.sin_port = htons(numpuerto); // puerto del servidor
+    
 
+    if (connect(sockservidor, (struct sockaddr *) &dirservidor, sizeof(dirservidor)) < 0){
+        perror("\nNo se pudo conectar al servidor\n");
+        exit(EXIT_FAILURE);
+    }
 
-   socketCliente = socket(AF_INET, SOCK_STREAM, 0);
-   if(socketCliente < 0){
-       perror("\nNo se pudo crear el socket\n");
-       exit(EXIT_FAILURE);
-   }
+    if(recv(sockservidor, mensaje,MAX,0) < 0){
+        perror("\nNo se pudo recibir el mensaje\n");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("\nMensaje recibido: %s\n", mensaje);
+    }
 
-
-  
-
+    close(sockservidor);
 
    return 0;
 }
